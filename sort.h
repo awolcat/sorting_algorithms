3ff1cd01fdda2799600aa80857fc5cb2baae0f19
Albert <albertirura11@gmail.com>
2023-06-26 05:57:10 -0700

    updated header file

--- sort.h
+++ sort.h
@@ -20,9 +20,16 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-
+/* bubble sort */
 void bubble_sort(int *array, size_t size);
+/* insertion sort */
 void insertion_sort_list(listint_t **list);
 void swap(listint_t **head, listint_t *src, listint_t *dest, int *len);
+/*selection sort */
 void selection_sort(int *array, size_t size);
+/* quick sort */
+void quick_sort(int *array, size_t size);
+void quick_sort_r(int *array, int lb, int ub, size_t *len);
+size_t partition(int *arr, size_t lb, size_t ub, size_t *len);
+void swap_arr(int *arr_1, int *arr_2);
 #endif
